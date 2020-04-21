4269f16d5a9f9856500e08151d44a570b23c07f2
mroso <mariana.roso.mol@gmail.com>
2020-04-21 18:16:37 -0300

    commit tarea

---
+++ DateFormat/DateFormat/obj/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DateFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("La fecha en el nuevo formato es:" + " " + DateFormat("10/11/1977"));
+        }
+
+        static string DateFormat (String fecha)
+        {
+            
+            return  fecha.Substring(6,4) + "-" + fecha.Substring(3,2) + "-" + fecha.Substring(0,2);  
+            
+        }
+    }
+}
